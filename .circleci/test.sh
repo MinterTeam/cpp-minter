e3e8927929f3dc90c353cf243a31e5952391035c
Eduard Maximovich <edward.vstock@gmail.com>
2020-10-19 02:59:13 +0300

    fixed conan remote adding, fixed test script

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,6 +3,6 @@
 set -e
 mkdir -p _build && cd _build
 rm -rf ./*
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_CONAN=Off -DMINTER_TX_TEST=On
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_CONAN=On -DMINTER_TX_TEST=On
 cmake --build . --target minter_tx-test
 ./bin/minter_tx-test
