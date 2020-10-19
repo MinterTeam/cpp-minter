3f14b023e77bfce3eabb4356dc1bc8dd4af8d254
Eduard Maximovich <edward.vstock@gmail.com>
2020-10-19 03:02:30 +0300

    --build=missing for conan deps

--- .circleci/conan_deps.sh
+++ .circleci/conan_deps.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-conan install toolbox/3.2.1@edwardstock/latest -s build_type=Debug
-conan install toolbox/3.2.1@edwardstock/latest -s build_type=Release
+conan install toolbox/3.2.1@edwardstock/latest -s build_type=Debug --build=missing
+conan install toolbox/3.2.1@edwardstock/latest -s build_type=Release --build=missing
 
-conan install bip39/2.1.0@edwardstock/latest -s build_type=Debug
-conan install bip39/2.1.0@edwardstock/latest -s build_type=Release
+conan install bip39/2.1.0@edwardstock/latest -s build_type=Debug --build=missing
+conan install bip39/2.1.0@edwardstock/latest -s build_type=Release --build=missing
 
-conan install bigmath/1.0.0@edwardstock/latest -s build_type=Debug
-conan install bigmath/1.0.0@edwardstock/latest -s build_type=Release
+conan install bigmath/1.0.0@edwardstock/latest -s build_type=Debug --build=missing
+conan install bigmath/1.0.0@edwardstock/latest -s build_type=Release --build=missing
 
-conan install secp256k1_java/1.0.0@edwardstock/latest -s build_type=Debug
-conan install secp256k1_java/1.0.0@edwardstock/latest -s build_type=Release
+conan install secp256k1_java/1.0.0@edwardstock/latest -s build_type=Debug --build=missing
+conan install secp256k1_java/1.0.0@edwardstock/latest -s build_type=Release --build=missing
 
-conan install gtest/1.8.1@bincrafters/stable -s build_type=Debug
+conan install gtest/1.8.1@bincrafters/stable -s build_type=Debug --build=missing
