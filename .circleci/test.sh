e86627658ab603b35651a08f76814e0baa767ffd
Eduard Maximovich <edward.vstock@gmail.com>
2020-10-19 03:00:26 +0300

    fixed test script again

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,6 +3,6 @@
 set -e
 mkdir -p _build && cd _build
 rm -rf ./*
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_CONAN=On -DMINTER_TX_TEST=On
-cmake --build . --target minter_tx-test
-./bin/minter_tx-test
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_CONAN=On -DENABLE_TEST=On
+make -j4
+make test
